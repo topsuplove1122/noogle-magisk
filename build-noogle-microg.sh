35138e9f88e672fc87a56f89d6eed11c2aea2868
SelfRef <git@selfref.dev>
2025-04-01 12:15:36 +0200

    Improve build scripts

--- build-noogle-microg.sh
+++ build-noogle-microg.sh
@@ -1,23 +1,40 @@
 #!/bin/sh
 
+apk_dir=apk
+
 echo "[P] Building module noogle-microg..."
 
-if [ ! -f apk/GmsCore.apk ] || [ ! -f apk/Phonesky.apk ]; then
-	echo "[W] microG APKs not found, pulling latest..."
-	./pull-latest-microg.sh
-	if [ $? -ne 0 ]; then
-		exit 1
+for apk in "com.google.android.gms" "com.android.vending" "com.google.android.gsf"; do
+	if ls "$apk_dir/$apk"* 1> /dev/null 2>&1; then
+		echo "[I] $apk: PRESENT"
+	else
+		echo "[W] $apk: MISSING"
 	fi
+done
+
+apk_count=$(ls "$apk_dir/com.google.android.gms"* "$apk_dir/com.android.vending"* "$apk_dir/com.google.android.gsf"* 2>/dev/null | wc -l)
+if [ "$apk_count" -lt 3 ]; then
+	echo "[E] Missing one or more required APKs in $apk_dir/, use ./pull-latest-microg.sh to download them or do it manually"
+	exit 1
+elif [ "$apk_count" -gt 3 ]; then
+	echo "[E] Too many APKs, the $apk_dir/ directory must contain exactly 3 APK files"
+	exit 1
 fi
 
-cp apk/GmsCore.apk module/system/product/priv-app/GmsCoreMicroG/GmsCore.apk
-cp apk/Phonesky.apk module/system/product/priv-app/PhoneskyMicroG/Phonesky.apk
+echo "[P] Copying APKs to module directory..."
+cp apk/com.google.android.gms* module/system/product/priv-app/GmsCoreMicroG/GmsCore.apk
+cp apk/com.android.vending* module/system/product/priv-app/PhoneskyMicroG/Phonesky.apk
+cp apk/com.google.android.gsf* module/system/product/priv-app/GoogleServicesFrameworkMicroG/GoogleServicesFramework.apk
+
+module_version="$(jq -r '.version' update-microg.json)"
+echo "[I] Module version: $module_version"
+module_filename="noogle-microg-v$module_version.zip"
 
 echo "[P] Compressing module archive..."
 mkdir -p dist
-rm -f dist/noogle-microg.zip
+rm -f "dist/$module_filename"
 cd module
-zip -q -r ../dist/noogle-microg.zip . -x **/.gitkeep
+zip -q -r ../dist/$module_filename . -x **/.gitkeep
 cd -
 
 echo "[I] Module noogle-microg built successfully!"
--- pull-latest-microg.sh
+++ pull-latest-microg.sh
@@ -1,27 +1,81 @@
 #!/bin/sh
 
-echo "[P] Pulling latest microG APKs..."
-
 repo_url=https://microg.org/fdroid/repo
+apk_dir=apk
+wait_time=15
 
-rm -rf apk
-mkdir apk
+mkdir -p "$apk_dir"
 
+echo "[P] Pulling latest microG APKs..."
 repo_index=$(curl -s $repo_url/index-v2.json)
+pulled_apk=""
 
 latest_gms=$(echo "$repo_index" | jq -r '.packages["com.google.android.gms"].versions | to_entries | sort_by(.value.added) | last.value.file.name')
-
 echo "[I] Latest GmsCore version: $latest_gms"
 
-latest_vending=$(echo "$repo_index" | jq -r '.packages["com.android.vending"].versions | to_entries | sort_by(.value.added) | last.value.file.name')
+gms_apk="$apk_dir$latest_gms"
+if [ -f "$gms_apk" ] && [ $(stat -c%s "$gms_apk") -gt 1000 ]; then
+	echo "[I] microG Services already pulled, skipping..."
+else
+	rm -f "$apk_dir/com.google.android.gms*"
+	echo "[P] Pulling latest microG Services..."
+	echo "[I] Waiting for 3 seconds to avoid rate limiting..."
+	sleep 3 # To avoid rate limiting
 
+	curl -L --progress-bar $repo_url$latest_gms -o "$gms_apk"
+	if [ $(stat -c%s "$gms_apk") -lt 1000 ]; then
+		rm -f "$gms_apk"
+		echo "[E] Failed to pull latest microG Services, probbaly because requests are rate limited. Try again in a moment."
+		exit 1
+	else
+		pulled_apk=1
+	fi
+fi
+
+latest_vending=$(echo "$repo_index" | jq -r '.packages["com.android.vending"].versions | to_entries | sort_by(.value.added) | last.value.file.name')
 echo "[I] Latest Phonesky version: $latest_vending"
 
-curl -Ls $repo_url/$latest_gms -o apk/GmsCore.apk
-curl -Ls $repo_url/$latest_vending -o apk/Phonesky.apk
+vending_apk="$apk_dir$latest_vending"
+if [ -f "$vending_apk" ] && [ $(stat -c%s "$vending_apk") -gt 1000 ]; then
+	echo "[I] microG Companion already pulled, skipping..."
+else
+	rm -f "$apk_dir/com.android.vending*"
+	echo "[P] Pulling latest microG Companion..."
+	if [ $pulled_apk ]; then
+	echo "[I] Waiting for $wait_time seconds to avoid rate limiting..."
+		sleep $wait_time # To avoid rate limiting
+	fi
 
-if [ $? -ne 0 ]; then
-	echo "[E] Failed to pull latest microG APKs"
-	exit 1
+	curl -L --progress-bar $repo_url$latest_vending -o "$vending_apk"
+	if [ $(stat -c%s "$vending_apk") -lt 1000 ]; then
+		rm -f "$vending_apk"
+		echo "[E] Failed to pull latest microG Companion, probbaly because requests are rate limited. Try again in a moment."
+		exit 1
+	else
+		pulled_apk=1
+	fi
 fi
-echo "[I] Pulled latest microG APKs"
+
+latest_gsf=$(echo "$repo_index" | jq -r '.packages["com.google.android.gsf"].versions | to_entries | sort_by(.value.added) | last.value.file.name')
+echo "[I] Latest GoogleServicesFramework version: $latest_gsf"
+
+gsf_apk="$apk_dir$latest_gsf"
+if [ -f "$gsf_apk" ] && [ $(stat -c%s "$gsf_apk") -gt 1000 ]; then
+	echo "[I] microG Companion already pulled, skipping..."
+else
+	rm -f "$apk_dir/com.google.android.gsf*"
+	echo "[P] Pulling latest microG Services Framework Proxy..."
+	if [ $pulled_apk ]; then
+	echo "[I] Waiting for $wait_time seconds to avoid rate limiting..."
+		sleep $wait_time # To avoid rate limiting
+	fi
+
+	curl -L --progress-bar $repo_url$latest_gsf -o "$gsf_apk"
+	if [ $(stat -c%s "$gsf_apk") -lt 1000 ]; then
+		rm -f "$gsf_apk"
+		echo "[E] Failed to pull latest microG Services Framework Proxy, probbaly because requests are rate limited. Try again in a moment."
+		exit 1
+	fi
+fi
+
+echo "[I] All microG APKs are ready!"
