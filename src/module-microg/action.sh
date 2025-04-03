00fbc55418206bce1fc82562e056cb36ef5de800
SelfRef <git@selfref.dev>
2025-04-03 05:26:09 +0200

    Update action script

---
+++ src/module-microg/action.sh
@@ -0,0 +1,17 @@
+source ./common.sh
+
+echo "[P] Module self-check..."
+
+echo
+echo "[W] microG APKs will be restored to original versions."
+
+echo
+remove_package_updates
+
+echo
+echo
+grant_microg_permissions
+
+echo
+echo
+echo "[I] Done fixing module."
