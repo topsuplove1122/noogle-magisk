9534c3e1404e72aa75505f945484782b675dfb5d
SelfRef <git@selfref.dev>
2025-04-04 11:06:47 +0200

    Fix typo

--- scripts/install-noogle.sh
+++ scripts/install-noogle.sh
@@ -12,7 +12,7 @@ if [ "$skip_confirmations" = false ]; then
     echo -n 'Install?'
     read
 fi
-adb push dist/noogle-microg-v$module_version.zip /sdcard/
+adb push dist/noogle-microg-$module_version.zip /sdcard/
 adb shell su -c "magisk --install-module /sdcard/noogle-microg-$module_version.zip"
 if [ "$skip_confirmations" = false ]; then
     echo -n 'Reboot?'
